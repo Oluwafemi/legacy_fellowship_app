bcb1143761ba5bbf135417de392d18250b3291f6
Oluwafemi A <favourfield@gmail.com>
2013-02-01 19:25:22 +0100

    Application Bootstrapped

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Legacydatabase::Application.config.secret_token = '845e2a5f13af49eae1b1cb1eb93d53c1072f1fcf25fbec3d83e4ca28c834e38b9589001dff5d885568c9e7f0847f79a4f27061baf4340e3e2e6a5dd3ea09041e'
